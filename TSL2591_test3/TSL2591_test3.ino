81aaf1ac1fb2d4694d29b577f5cb37a665cb2974
Kartik0611 <kartikhegde0611@gmail.com>
2018-11-02 00:22:28 +0530

    Updated TSL2591_test3

--- TSL2591_test3/TSL2591_test3.ino
+++ TSL2591_test3/TSL2591_test3.ino
@@ -5,6 +5,7 @@
 #include <Wire.h>
 #include <Adafruit_Sensor.h>
 #include "Adafruit_TSL2591.h"
+#define LED_BUILTIN 13
 
 // Example for demonstrating the TSL2591 library - public domain!
 
@@ -15,7 +16,6 @@
 
 Adafruit_TSL2591 tsl = Adafruit_TSL2591(2591); // pass in a number for the sensor identifier (for your use later)
 
-
 /**************************************************************************/
 /*
     Configures the gain and integration time for the TSL2591
@@ -24,8 +24,8 @@ Adafruit_TSL2591 tsl = Adafruit_TSL2591(2591); // pass in a number for the senso
 
 void configureSensor(void)
 {
-  tsl.setGain(TSL2591_GAIN_LOW);    // 1x gain (bright light)
-  tsl.setTiming(TSL2591_INTEGRATIONTIME_200MS);  // shortest integration time (bright light) , can also be st to 100MS
+  
+ 
   
 }
 
@@ -48,34 +48,19 @@ void setup(void)
     Serial.println(F("No sensor found ... check your wiring?"));
     while (1);
   }
-  configureSensor();
-}
-
-/**************************************************************************/
-/*
-    Shows how to perform a basic read on visible, full spectrum or
-    infrared light (returns raw 16-bit ADC values)
-*/
-/**************************************************************************/
-
-void simpleRead(void)
-{
-  uint16_t x = tsl.getLuminosity(TSL2591_VISIBLE);
-  Serial.print(F("[ ")); Serial.print(millis()); Serial.print(F(" ms ] "));
-  Serial.print(F("Luminosity: "));
-  Serial.println(x, DEC);
+  tsl.setGain(TSL2591_GAIN_LOW);    // 1x gain (bright light)
+  tsl.setTiming(TSL2591_INTEGRATIONTIME_200MS);  // shortest integration time (bright light) , can also be st to 100MS
 }
 
 
-
-/**************************************************************************/
-/*
-    Arduino loop function, called once 'setup' is complete (your own code
-    should go here)
-*/
-/**************************************************************************/
 void loop(void) 
 { 
-  simpleRead(); 
-  delay(200);
+  while(true)
+  {
+    uint16_t x = tsl.getLuminosity(TSL2591_VISIBLE);
+    Serial.print(F("[ ")); Serial.print(millis()); Serial.print(F(" ms ] "));
+    Serial.print(F("Luminosity: "));
+    Serial.println(x, DEC);
+    delay(200);
+  }
 }
