cd27ac34074e9716e378b388c060f4b92aabc8e3
Kartik0611 <kartikhegde0611@gmail.com>
2018-11-02 00:24:21 +0530

    Added TSL2591_bubblesize
    This is same as TSL2591_test3 program. It has to be updated

---
+++ TSL2591_bubblesize/TSL2591_bubblesize.ino
@@ -0,0 +1,66 @@
+/* TSL2591 Digital Light Sensor */
+/* Dynamic Range: 600M:1 */
+/* Maximum Lux: 88K */
+
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include "Adafruit_TSL2591.h"
+#define LED_BUILTIN 13
+
+// Example for demonstrating the TSL2591 library - public domain!
+
+// connect SCL to I2C Clock
+// connect SDA to I2C Data
+// connect Vin to 3.3-5V DC
+// connect GROUND to common ground
+
+Adafruit_TSL2591 tsl = Adafruit_TSL2591(2591); // pass in a number for the sensor identifier (for your use later)
+
+/**************************************************************************/
+/*
+    Configures the gain and integration time for the TSL2591
+*/
+/**************************************************************************/
+
+void configureSensor(void)
+{
+  
+ 
+  
+}
+
+
+/**************************************************************************/
+/*
+    Program entry point for the Arduino sketch
+*/
+/**************************************************************************/
+
+void setup(void) 
+{
+  Serial.begin(9600);
+  if (tsl.begin()) 
+  {
+    Serial.println(F("Found a TSL2591 sensor"));
+  } 
+  else 
+  {
+    Serial.println(F("No sensor found ... check your wiring?"));
+    while (1);
+  }
+  tsl.setGain(TSL2591_GAIN_LOW);    // 1x gain (bright light)
+  tsl.setTiming(TSL2591_INTEGRATIONTIME_200MS);  // shortest integration time (bright light) , can also be st to 100MS
+}
+
+
+void loop(void) 
+{ 
+  while(true)
+  {
+    uint16_t x = tsl.getLuminosity(TSL2591_VISIBLE);
+    Serial.print(F("[ ")); Serial.print(millis()); Serial.print(F(" ms ] "));
+    Serial.print(F("Luminosity: "));
+    Serial.println(x, DEC);
+    delay(200);
+  }
+}
